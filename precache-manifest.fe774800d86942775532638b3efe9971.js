28ddd56d1c89925c6c3603e1bffbda6687f56758
songyii1031 <127111505+songyii1031@users.noreply.github.com>
2025-01-08 16:39:44 +0900

    Add files via upload

---
+++ precache-manifest.fe774800d86942775532638b3efe9971.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4be5460b4587724b95671fa928b4cd7c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a112bd6ae76c69625e63",
+    "url": "/static/css/main.0572d2d0.chunk.css"
+  },
+  {
+    "revision": "dc534d40cc55a926afdf",
+    "url": "/static/js/2.0e9b2eab.chunk.js"
+  },
+  {
+    "revision": "f1c657f38def6eca0cb82a34c1715086",
+    "url": "/static/js/2.0e9b2eab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84ef2487606ecf312c35",
+    "url": "/static/js/3.55520b69.chunk.js"
+  },
+  {
+    "revision": "a112bd6ae76c69625e63",
+    "url": "/static/js/main.b2b81819.chunk.js"
+  },
+  {
+    "revision": "c077578d30e81441d715",
+    "url": "/static/js/runtime-main.348bfaec.js"
+  },
+  {
+    "revision": "f18063958be2e76b9d2fab3d791160cf",
+    "url": "/static/media/main_img.f1806395.jpg"
+  }
+]);
